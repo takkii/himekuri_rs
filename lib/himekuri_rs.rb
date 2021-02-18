70598b92eec0774ba05f649d146b9a9f543bfe6f
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-02-18 18:49:12 +0900

    Update.

--- lib/himekuri_rs.rb
+++ lib/himekuri_rs.rb
@@ -10,7 +10,7 @@ module Hello
   ruby_version = (RUBY_VERSION).to_s
   ruby_folder_version = ("3.0.0").to_s
   version = (HimekuriRs::VERSION).to_s
-  himekuri_ts = "himekuri_rs-".to_s + version.to_s
+  himekuri_rs = "himekuri_rs-".to_s + version.to_s
   
   extend FFI::Library
   ffi_lib "$HOME/.rbenv/versions/" + ruby_version + "/lib/ruby/gems/" + ruby_folder_version + "/gems/" + himekuri_rs +  "/lib/" + "libhimekuri_rust.dylib"
