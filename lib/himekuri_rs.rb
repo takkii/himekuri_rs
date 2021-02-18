147903ea802cddac1e1e8f414f96501fb59ab8f6
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-02-18 18:07:52 +0900

    Update.

--- lib/himekuri_rs.rb
+++ lib/himekuri_rs.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+lib = File.expand_path('lib', __dir__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
 require "ffi"
 
 module Hello
   extend FFI::Library
-  ffi_lib "./lib/libhimekuri_rust.dylib"
+  ffi_lib "libhimekuri_rust.dylib"
   attach_function :himekuri, [], :void
 end
